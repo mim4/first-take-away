17df75aab7cc51a9f31cad35e10f3de5c6cdcdc1
mim4 <milundain98@gmail.com>
2021-03-09 11:53:28 +0100

    Added navbarPage

--- app.R
+++ app.R
@@ -1,8 +1,13 @@
 library(shiny)
 library(shinythemes)
+require(shinyjs)
 # Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinytheme("spacelab"),
-                h1="hola")
+ui <- navbarPage("First Take Away",
+                 theme = shinytheme("spacelab"),
+                 tabPanel("Data"),
+                 tabPanel("Machine Learning"),
+                 useShinyjs()
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) { }
